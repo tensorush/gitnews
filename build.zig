f0d71bc74bc24d2584626401f6aeb5426b7ad365
Jora Troosh <tensorush@gmail.com>
2024-01-03 16:12:05 +0300

    fix: remove redundant output line

--- build.zig
+++ build.zig
@@ -9,7 +9,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = std.Build.FileSource.relative("src/main.zig"),
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
-        .version = .{ .major = 1, .minor = 0, .patch = 2 },
+        .version = .{ .major = 1, .minor = 0, .patch = 3 },
     });
     b.installArtifact(exe);
 
--- src/main.zig
+++ src/main.zig
@@ -18,14 +18,13 @@ pub fn main() Error!void {
 
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
+
     var thread_safe_allocator = std.heap.ThreadSafeAllocator{ .child_allocator = arena.allocator() };
     const allocator = thread_safe_allocator.allocator();
 
     const std_out = std.io.getStdOut();
     var buf_writer = std.io.bufferedWriter(std_out.writer());
     const writer = buf_writer.writer();
-
-    try writer.writeAll("Content-Type: text/plain\n\n");
 
     var client = std.http.Client{ .allocator = allocator };
     defer client.deinit();
